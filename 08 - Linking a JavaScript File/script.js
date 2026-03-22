e27689913d47ff4c1ec9a3b2dd9abc72ca6b2ec1
ushanAlwis98 <ushanloshitha@gmail.com>
2026-03-22 23:14:28 +0530

    Control Structure

--- 08 - Linking a JavaScript File/script.js
+++ 08 - Linking a JavaScript File/script.js
@@ -1,17 +1,10 @@
-let js = "amazing";
-if (js === "amazing") alert(`JavaScript is ${js}!`);
+let ushan = 27;
+let dilan = 28;
 
-40 + 8 + 23 - 10;
-console.log(40 + 8 + 23 - 10);
-
-let years3 = 3;
-
-// console.log(years3);
-
-const javaScriptIsFun = true;
-console.log(javaScriptIsFun);
-let year = 2024;
-let name = "John";
-
-let result = 10 + 5 * 2;
-console.log(result);
+if (ushan > dilan) {
+  console.log("Ushan is older than Dilan");
+} else if (ushan < dilan) {
+  console.log("Dilan is older than Ushan");
+} else {
+  console.log("Ushan and Dilan are the same age");
+}
